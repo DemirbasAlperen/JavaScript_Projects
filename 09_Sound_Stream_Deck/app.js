e84278f6627c50c667aef455fecd72854bce5f8c
Alperen Demirbas <alperendmrbs38@gmail.com>
2025-01-18 14:57:39 +0300

    dokuzuncu Js projem

---
+++ 09_Sound_Stream_Deck/app.js
@@ -0,0 +1,26 @@
+// Js birinci kısım
+
+const sounds = ["applause", "boo", "gasp", "tada", "lalala", "wrong"];   // sounds isimli bir dizi oluşturuldu
+
+sounds.forEach((sound) => {   // sounds içerisine girip hepsini gezdik
+    const btn = document.createElement('button');    // button elementi oluşturduk
+    btn.classList.add("btn");   // oluşturduğumuz buton elementine btn isimli class ekledik
+
+    btn.innerText = sound;    // btn innerText lerine girdik ve soundlarına ulaştık
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").appendChild(btn);   // buttons id sini seçtik ve altına bir düğüm(btn isimli) daha ekledik
+})
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
